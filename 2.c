1bdaae59d89103665cfb3d71d72adf6bcfa9e97c
TomLyu <tomlyu@ami.com.cn>
2018-01-25 10:57:06 +0800

    this is change3.

--- 2.c
+++ 2.c
@@ -4,5 +4,6 @@ int main()
 {
 	int a=1;
 	printf("Hello,World\n");
+	printf("this is change3\n");
 	return 0;
 }
---
+++ 3.cpp
@@ -0,0 +1,7 @@
+include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"This is a snowy day.2018.1.25";
+	return 0;
+}
