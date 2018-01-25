a54f32a61e1883faa8800277abe3c1fab64a0797
TomLyu <tomlyu@ami.com.cn>
2018-01-25 11:05:25 +0800

    the medified for change3_1.

--- 2.c
+++ 2.c
@@ -5,5 +5,6 @@ int main()
 	int a=1;
 	printf("Hello,World\n");
 	printf("this is change3\n");
+	printf("this is change3_1;");
 	return 0;
 }
--- 3.cpp
+++ 3.cpp
@@ -1,5 +1,11 @@
 include<iostream>
 using namespace std;
+class a
+{
+	public:
+			int a;
+			int ang();	
+}
 int main()
 {
 	cout<<"This is a snowy day.2018.1.25";
